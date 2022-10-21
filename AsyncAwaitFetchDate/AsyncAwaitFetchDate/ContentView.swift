2e579161d14a8b083cc99a1d3f6d74b8197a5187
Yu Zhu <yu.zhu@liebherr.com>
2022-10-21 08:05:35 +0200

    fetch date

--- AsyncAwaitFetchDate/AsyncAwaitFetchDate/ContentView.swift
+++ AsyncAwaitFetchDate/AsyncAwaitFetchDate/ContentView.swift
@@ -17,6 +17,20 @@ struct CurrentDate: Codable, Identifiable {
 }
 
 struct ContentView: View {
+    
+    private func getDate() async throws -> CurrentDate {
+        // resolve url
+        guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError("url not available")
+        }
+        // fetch api data
+        let (data, _) = try await URLSession.shared.data(from: url)
+        // decode data to model
+        let currentDate = try JSONDecoder().decode(CurrentDate.self, from: data)
+        
+        return currentDate
+    }
+    
     var body: some View {
         NavigationView {
             List(1...10, id:\.self) { i in
