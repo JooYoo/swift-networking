dfcf4e0ae254deb9fd4ee1cbe539f8a829b4a291
JooYoo <jooyoo@outlook.com>
2022-10-30 11:04:07 +0100

    project init

---
+++ NewsSource/NewsSource/NewsSourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSourceApp.swift
+//  NewsSource
+//
+//  Created by Yu on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct NewsSourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
