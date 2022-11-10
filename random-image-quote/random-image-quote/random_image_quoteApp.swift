b30e0e423efa45f2f80965576794155b418a18eb
JooYoo <jooyoo@outlook.com>
2022-11-10 05:24:24 +0100

    random-image-quote init

---
+++ random-image-quote/random-image-quote/random_image_quoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_image_quoteApp.swift
+//  random-image-quote
+//
+//  Created by Yu on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct random_image_quoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
